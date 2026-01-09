0158eaaa3206eff16664296dd9b61c5dab94a367
Sergi Izquierdo <sergi.izquierdo@estudiants.urv.cat>
2026-01-09 15:02:47 +0100

    Initial commit

---
+++ coches-app/src/main/java/daaw_examen/coches_app/CochesAppApplication.java
@@ -0,0 +1,13 @@
+package daaw_examen.coches_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CochesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CochesAppApplication.class, args);
+	}
+
+}
---
+++ coches-app/src/test/java/daaw_examen/coches_app/CochesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package daaw_examen.coches_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CochesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
